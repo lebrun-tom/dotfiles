85c55c1e824e4148b846651ecc8d88431812f6de
Tom LEBRUN <lebrun.tom73@gmail.com>
2022-05-22 12:34:21 +0200

    feat : add tray icons

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -11,9 +11,21 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Manage multiple monitors
 if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload top &
+  IFS=$'\n'  # must set internal field separator to avoid dumb
+  for m in $(xrandr --query | grep " connected"); do
+    mon=$(cut -d" " -f1 <<< "$m")
+    status=$(cut -d" " -f3 <<< "$m")
+    tray_pos=""  
+    divider="" 
+
+    if [ "$status" == "primary" ]; then      
+        divider="|"                      
+        tray_pos="right"                                              
+    fi  
+
+    MONITOR=$mon TRAY_POS=$tray_pos DIVIDER=$divider polybar -r top &
   done
+    unset IFS  # avoid mega dumb by resetting the IFS
 else
   polybar --reload top &
 fi
